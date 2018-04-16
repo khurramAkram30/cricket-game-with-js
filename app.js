41b46521764abdda4aad29f2c856c3ddbcc3b787
khurramAkram30 <khurram.akram43@gmail.com>
2018-04-16 10:19:57 +0500

    com

---
+++ app.js
@@ -0,0 +1,115 @@
+
+
+
+var teamscore = 0;
+var teams1total = 0;
+var teams2total = 0;
+var balls = 0;
+var over = 0;
+var player = 1;
+var currentteam = 1;
+
+
+var pscore = document.getElementById('pscore');
+var var_team = document.getElementById('team');
+var var_players = document.getElementById('players');
+var var_score = document.getElementById('score');
+
+
+function circketmatch()
+{
+
+    (function(){
+        var myDiv = document.getElementById("myDiv"),
+    
+          show = function(){
+            myDiv.style.display = "block";
+            setTimeout(hide, 3000); // 5 seconds
+          },
+    
+          hide = function(){
+            myDiv.style.display = "none";
+          };
+    
+        show();
+      })();
+    
+
+var rand = Math.floor(Math.random()*7);
+var comment = '';
+
+    
+    if(rand == 7)
+    {
+        teamscore++;
+        comment += " No Ball...";
+        comment += " ----TeamScore " + currentteam + " " + teamscore + " ";
+    }
+    else{
+        balls ++;
+        teamscore += rand;
+
+        comment += " Player "+player+ " runs increase by " + rand;
+        comment += " ----TeamScore " +  currentteam + " " + teamscore + " ";
+
+        if (rand == 0)
+        {
+            player++;
+            comment += "Your Player is out..";
+            //comment += " total score is " + teamscore + " ";
+        }
+    }
+
+    
+
+    if(balls == 6){
+        balls = 0;
+        over++;
+
+        comment += " " + over + " Over Completed";
+    }
+
+    if (over == 2 || player == 3){
+
+
+        if(currentteam == 1)
+        {
+            teams1total = teamscore;
+            comment += " Team "+ currentteam +"  " + over + " Over Completed on " + (parseInt(player)-1) + " Wickekts gone!! Total Score" + teams1total;
+            currentteam++;
+        }
+        else if(currentteam == 2){
+            teams2total = teamscore;
+            comment += " Team "+ currentteam +"  " + over + " Over Completed on " + (parseInt(player)-1) + " Wickekts gone!! Total Score" + teams2total;
+            
+        }
+
+        over =0;
+        player =1;
+        teamscore =0;
+        balls =0;
+
+    }
+    
+    if(teams1total !==0 && teams2total !==0)
+    {
+        
+        
+            if (teams1total >= teams2total) { comment += "Team 1 Wins the Match...";}
+            if (teams2total >= teams1total) { comment += "Team 2 Wins the Match...";}
+            teams1total = 0;
+            teams2total = 0;        
+    }
+    //console.clear();
+    // console.log(teamscore + ' teamscore');
+    // console.log(balls + ' balls');
+    // console.log(over + ' over');
+    // console.log(player + ' player');
+
+    var_team.innerHTML = currentteam
+    var_players.innerHTML = player;
+    var_score.innerHTML = teamscore
+
+    pscore.innerHTML += "<br/>"+comment;
+    //console.log(comment);
+}
